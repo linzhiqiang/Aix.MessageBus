c19a8e8f6281e4d5f406f429d9d1d9c43f65862f
linzhiqiang <linzhiqiang@shandagames.com>
2019-05-06 19:16:02 +0800

    test

--- Samples/Sample/Program.cs
+++ Samples/Sample/Program.cs
@@ -46,7 +46,8 @@ namespace Sample
             var host = new HostBuilder()
                 .ConfigureHostConfiguration(builder =>
                 {
-                    builder.AddEnvironmentVariables(prefix: "Demo_");
+                    //https://www.cnblogs.com/subendong/p/8834902.html
+                    builder.AddEnvironmentVariables(prefix: "Demo_"); //配置环境变量 Demo_ENVIRONMENT Development/Staging/Production(默认值)
                 })
                 .ConfigureAppConfiguration((hostContext, config) =>
                 {
@@ -59,8 +60,14 @@ namespace Sample
                 })
                 .ConfigureServices((context, services) =>
                 {
-                    services.AddSingleton(options);
+                    //var environment = Environment.GetEnvironmentVariable("Demo_ENVIRONMENT");
+                    //if (context.HostingEnvironment.IsEnvironment("Development"))
+                    if (context.HostingEnvironment.IsDevelopment()) //IsStaging(),IsProduction()
+                    {
 
+                    }
+
+                    services.AddSingleton(options);
                     var kafkaMessageBusOptions = context.Configuration.GetSection("kafka").Get<KafkaMessageBusOptions>();
                     kafkaMessageBusOptions.ClientMode = options.Mode;//这里方便测试，以命令行参数为准
                     services.AddKafkaMessageBus(kafkaMessageBusOptions);
@@ -78,6 +85,7 @@ namespace Sample
                     }
 
                 });
+
             Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff")}开始");
             host.RunConsoleAsync().Wait();
             Console.WriteLine("服务已退出");
@@ -98,7 +106,7 @@ namespace Sample
         private static void AddKafkaMessageBus(IServiceCollection services, ClientMode mode)
         {
             var bootstrapServers = "192.168.111.132:9092,192.168.111.132:9093,192.168.111.132:9094";// com 虚拟机
-           // bootstrapServers = "192.168.72.132:9092,192.168.72.132:9093,192.168.72.132:9094";//home 虚拟机
+                                                                                                    // bootstrapServers = "192.168.72.132:9092,192.168.72.132:9093,192.168.72.132:9094";//home 虚拟机
 
             var options = new KafkaMessageBusOptions
             {
@@ -119,7 +127,7 @@ namespace Sample
                     GroupId = "kafka-messagebus",
                     BootstrapServers = bootstrapServers,
                     AutoOffsetReset = AutoOffsetReset.Earliest,
-                   // EnableAutoCommit = false,
+                    // EnableAutoCommit = false,
                     AutoCommitIntervalMs = 5000, //自动提交偏移量间隔 ，每5秒同步一次,当再均衡时，如果有消费者一直没有poll，会等到所有的消费者都poll之后才再均衡处理
                     CancellationDelayMaxMs = 1000 //poll等待时间，如果自己 consumer.Consume(TimeSpan.FromSeconds(1));写的时间就不用这个配置了
 
